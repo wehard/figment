287fe5098d1759adef96cc002844ff383f9c5ac6
Willehard Korander <willehard.korander@unity3d.com>
2022-10-09 00:39:00 +0300

    Tailwindcss eslint prettier

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect, useRef } from "react";
-import Renderer from "./Renderer";
-import "./App.css";
+import { useState, useEffect, useRef } from 'react';
+import Renderer from './Renderer';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App bg-red-200'>
       <h1>Figment</h1>
       <Renderer></Renderer>
     </div>
