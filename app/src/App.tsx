24f5dd66d2a2cb91d874779623267ac2e75cebd6
Willehard Korander <willehard.korander@unity3d.com>
2022-10-10 22:10:44 +0300

    Tailwind config

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div className='flex h-12 w-full border-b border-neutral-700 bg-neutral-800'>
         <button>Insert</button>
       </div>
-      <div className='flex flex-row h-screen'>
+      <div className='flex h-screen flex-row'>
         <div className='flex w-2/12 border-r border-neutral-700 bg-neutral-800'></div>
         <div>
           <Renderer />
