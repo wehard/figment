61bce7686dc56bbc0724cbf8dc07ded7f1e9b7f9
Willehard Korander <willehard.korander@unity3d.com>
2022-10-12 00:09:17 +0300

    Some panning

--- renderer/include/OrthoCamera.h
+++ renderer/include/OrthoCamera.h
@@ -14,9 +14,13 @@ public:
     glm::vec3 GetPosition() { return m_Position; }
     void SetPosition(glm::vec3 position);
     float GetZoom() { return m_Zoom; }
-    void Update();
+    void UpdateViewMatrix();
     void Zoom(float delta, glm::vec2 mousePosition);
     void OnResize(float width, float height);
+    void OnUpdate(glm::vec2 mousePosition);
+    glm::vec2 ScreenToWorldSpace(int sx, int sy);
+    void BeginPan(glm::vec2 mousePosition);
+    void EndPan();
 
 private:
     glm::mat4 m_ProjectionMatrix;
@@ -27,4 +31,7 @@ private:
 
     float m_AspectRatio = 1.0f;
     float m_Zoom = 1.0f;
+    glm::vec2 m_ViewportSize = {1280.0f, 720.0f};
+    bool m_IsPanning = false;
+    glm::vec2 m_InitialMousePosition = {0.0f, 0.0f};
 };
