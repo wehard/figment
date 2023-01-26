e99147487730f7bdb8546013bd9ebbc3317f5b9c
Willehard Korander <willehard.korander@unity3d.com>
2023-01-26 22:49:38 +0200

    Resize

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,18 +3,31 @@ import Renderer from './Renderer';
 import './App.css';
 
 function App() {
+  const rref = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState<{ width: number; height: number }>({
+    width: 1280,
+    height: 400,
+  });
+
+  useEffect(() => {
+    if (rref.current) {
+      const s = getComputedStyle(rref.current);
+      setSize({ width: parseInt(s.width), height: parseInt(s.height) });
+    }
+  }, []);
+
   return (
     <div>
-      <Renderer />
-      {/* <div className='flex h-12 w-full border-b border-neutral-700 bg-neutral-800'>
+      <div className='flex h-12 w-full border-b border-neutral-700 bg-neutral-800'>
         <button>Insert</button>
       </div>
-      <div className='flex h-screen flex-row'>
-        <div className='flex w-2/12 border-r border-neutral-700 bg-neutral-800'></div>
-        <div>
+      <div className='flex h-full grow flex-row'>
+        <div className='w-2/12 grow-0 border-r border-neutral-700 bg-neutral-800'>HELLO</div>
+        <div ref={rref} className='grow bg-red-400'>
+          <Renderer width={size.width} height={size.height} />
         </div>
-        <div className='flex w-2/12 border-l border-neutral-700 bg-neutral-800'></div>
-      </div> */}
+        <div className='w-2/12 grow-0  border-l border-neutral-700 bg-neutral-800'>WORLD</div>
+      </div>
     </div>
   );
 }
--- app/src/Renderer.tsx
+++ app/src/Renderer.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import loadRenderer from './renderer/index.js';
 
-const Renderer = () => {
+const Renderer = (props: { width: number; height: number }) => {
   const [context, setContext] = useState<any>(null);
   const canvas = useRef<HTMLCanvasElement>(null);
 
@@ -18,27 +18,29 @@ const Renderer = () => {
     if (context !== null) {
       console.log('Renderer initialized!');
     }
-    const initWidth = window.innerWidth;
-    const initHeight = window.innerHeight;
+    const initWidth = 100; // window.innerWidth;
+    const initHeight = 100; //window.innerHeight;
     let Module = {
       canvas: canvas.current,
       arguments: [initWidth.toString(), initHeight?.toString()],
     };
     loadRenderer(Module).then((rendererContext: any) => {
       setContext(rendererContext);
-      // window.addEventListener('resize', handleResize);
+      window.addEventListener('resize', handleResize);
     });
     return () => {
-      // window.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <div className='absolute h-full w-full'>
+    <div className=''>
       <canvas
         className='webgl-canvas'
         id='canvas'
         ref={canvas}
+        width={100}
+        height={100}
         onContextMenu={(e) => e.preventDefault()}
       ></canvas>
     </div>
