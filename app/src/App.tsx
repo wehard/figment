63296133b5df04967fee9e0fc5ceb8f5ef49b482
Willehard Korander <willehard.korander@unity3d.com>
2023-02-11 01:01:05 +0200

    Remove sidebar and move toolbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,14 +5,14 @@ import './App.css';
 
 interface ToolbarProps {
   canvasContext: CanvasContext;
+  toggleShowEditor: () => void;
 }
 
 const Toolbar = (props: ToolbarProps) => {
   const canvasContext = props.canvasContext;
-  const [inputEnabled, setInputEnabled] = useState<boolean>(true);
 
   return (
-    <div className='flex h-12 w-full border-b border-neutral-700 bg-neutral-800'>
+    <div className='flex h-full w-12 flex-col border-b border-neutral-700 bg-neutral-900'>
       <button
         className='w-12 active:bg-neutral-700'
         onClick={() => {
@@ -34,21 +34,27 @@ const Toolbar = (props: ToolbarProps) => {
       <button className='w-12 active:bg-neutral-700'>5</button>
       <button className='w-12 active:bg-neutral-700'>6</button>
       <button className='w-12 active:bg-neutral-700'>7</button>
-      <button
-        className='w-12 active:bg-neutral-700'
-        onClick={() => {
-          if (canvasContext) {
-            const i = inputEnabled ? 0 : 1;
-            setInputEnabled(!inputEnabled);
-            canvasContext.setInputEnabled(i);
-          }
-        }}
-      >
-        8
+      <button className='w-12 active:bg-neutral-700' onClick={props.toggleShowEditor}>
+        E
       </button>
-      <span>
-        {'Input:'} {inputEnabled ? 'true' : 'false'}
-      </span>
+    </div>
+  );
+};
+
+const CodeEditor = () => {
+  const handleEditorChange = (value: string | undefined, ev: any) => {
+    console.log(value);
+  };
+
+  return (
+    <div className='h-[30rem] w-[60rem]'>
+      <Editor
+        defaultLanguage='glsl'
+        defaultValue='// some comment'
+        value='#'
+        theme={'vs-dark'}
+        onChange={handleEditorChange}
+      />
     </div>
   );
 };
@@ -67,11 +73,7 @@ const debounce = (func: () => void, ms: number) => {
 function App() {
   const renderDivRef = useRef<HTMLDivElement>(null);
   const [canvasContext, setCanvasContext] = useState<CanvasContext>();
-  const [size, setSize] = useState<{ width: number; height: number }>({ width: 320, height: 200 });
-
-  const handleEditorChange = (value: string | undefined, ev: any) => {
-    console.log(value);
-  };
+  const [showEditor, setShowEditor] = useState<boolean>(true);
 
   useEffect(() => {
     const debounceHandleResize = debounce(() => {
@@ -82,6 +84,7 @@ function App() {
         );
     }, 10);
 
+    debounceHandleResize();
     window.addEventListener('resize', debounceHandleResize);
     return () => {
       window.removeEventListener('resize', debounceHandleResize);
@@ -89,15 +92,16 @@ function App() {
   });
 
   return (
-    <div id='app' className='app h-screen w-screen overflow-x-hidden overflow-y-hidden bg-black'>
-      {canvasContext && <Toolbar canvasContext={canvasContext}></Toolbar>}
-      <div className='flex h-full flex-row '>
-        <div className='flex h-full w-[15rem] min-w-[15rem] flex-col content-start items-start justify-start border-r border-neutral-700 bg-neutral-800 p-2'>
-          <ul className='flex list-inside list-decimal flex-col flex-wrap content-start items-start justify-start justify-items-start'>
-            <li>Insert quad</li>
-            <li>Insert circle</li>
-          </ul>
-        </div>
+    <div id='app' className='app h-screen w-screen overflow-x-hidden  bg-black'>
+      <div className='flex h-full w-full flex-row '>
+        {canvasContext && (
+          <Toolbar
+            canvasContext={canvasContext}
+            toggleShowEditor={() => {
+              setShowEditor(!showEditor);
+            }}
+          />
+        )}
         <div ref={renderDivRef} className={'h-full w-full'}>
           <Renderer
             initialWidth={renderDivRef.current?.clientWidth ?? 1280}
@@ -105,15 +109,17 @@ function App() {
             registerCallback={setCanvasContext}
           />
         </div>
-        {/* <div className='fixed bottom-0 h-[30rem] w-[60rem] border-t-4 border-r-4 border-neutral-700'>
-          <Editor
-            defaultLanguage='glsl'
-            defaultValue='// some comment'
-            value='code'
-            theme={'vs-dark'}
-            onChange={handleEditorChange}
-          />
-        </div> */}
+        <div className='fixed bottom-0 right-0 w-[60rem] border-neutral-900 bg-neutral-900'>
+          <div className='flex items-start '>
+            <button
+              className='w-12 bg-neutral-500 active:bg-neutral-700'
+              onClick={() => setShowEditor(!showEditor)}
+            >
+              <span>{showEditor ? 'v' : '^'}</span>
+            </button>
+          </div>
+          {showEditor && <CodeEditor />}
+        </div>
       </div>
     </div>
   );
