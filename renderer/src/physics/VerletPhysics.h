eb54b7651abea3b08ff8dee360ad3bb085b24586
Willehard Korander <willehard.korander@unity3d.com>
2023-03-09 21:16:03 +0200

    Adjust friction

--- renderer/src/physics/VerletPhysics.h
+++ renderer/src/physics/VerletPhysics.h
@@ -9,7 +9,7 @@ class VerletPhysics
 private:
     float m_WorldWidth = 40;
     float m_WorldHeight = 20;
-    const float m_BounceFriction = 1.0;
+    const float m_BounceFriction = 0.9;
     const float m_Gravity = 0.1;
     const float m_Friction = 0.999;
 
