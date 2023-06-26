afeaa99fca732fb713383b468a867c11e299156e
Willehard Korander <willehard@gmail.com>
2023-06-26 21:31:16 +0300

    Add OpenGL framebuffer

---
+++ renderer/src/platform/opengl/OpenGLFramebuffer.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "GL/glew.h"
+#include <vector>
+#include <glm/vec4.hpp>
+
+struct FramebufferDesc
+{
+    GLuint m_Width;
+    GLuint m_Height;
+};
+
+struct FramebufferTextureDesc
+{
+    GLint m_InternalFormat;
+    GLenum m_Format;
+    GLenum m_Type;
+};
+
+enum class TextureFormat
+{
+    RGBA8 = GL_RGBA8,
+    RED_INTEGER = GL_RED_INTEGER,
+    Depth = GL_DEPTH24_STENCIL8
+
+};
+
+class OpenGLFramebuffer
+{
+public:
+    OpenGLFramebuffer(const FramebufferDesc &desc);
+    ~OpenGLFramebuffer();
+    void Recreate();
+    void Bind();
+    void Unbind();
+    void ClearAttachment(GLuint index, glm::vec4 clearColor, int clearId);
+    void Resize(uint32_t width, uint32_t height);
+    GLuint GetColorAttachmentId(uint32_t index = 0) const
+    {
+        return m_ColorAttachments[index];
+    }
+    int GetPixel(uint32_t attachmentIndex, int x, int y);
+
+private:
+    FramebufferDesc m_Desc;
+    GLuint m_FboID;
+    GLuint rbo_id;
+
+    std::vector<FramebufferTextureDesc> m_ColorAttachmentDescs;
+    FramebufferTextureDesc m_DepthAttachmentDesc;
+
+    std::vector<GLuint> m_ColorAttachments;
+    GLuint m_DepthAttachment;
+};
