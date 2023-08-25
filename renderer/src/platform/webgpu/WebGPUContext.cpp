07efa5aa6094fab0bb0ed0ded96cbb553cd74794
Willehard Korander <willehard@gmail.com>
2023-08-25 23:33:49 +0300

    Set error print callback

--- renderer/src/platform/webgpu/WebGPUContext.cpp
+++ renderer/src/platform/webgpu/WebGPUContext.cpp
@@ -6,9 +6,30 @@
 
 #include <cstdio>
 
+static void PrintWebGPUError(WGPUErrorType error_type, const char* message, void*)
+{
+    const char* error_type_lbl = "";
+    switch (error_type)
+    {
+        case WGPUErrorType_Validation:  error_type_lbl = "Validation"; break;
+        case WGPUErrorType_OutOfMemory: error_type_lbl = "Out of memory"; break;
+        case WGPUErrorType_Unknown:     error_type_lbl = "Unknown"; break;
+        case WGPUErrorType_DeviceLost:  error_type_lbl = "Device lost"; break;
+        default:                        error_type_lbl = "Unknown";
+    }
+    printf("%s error: %s\n", error_type_lbl, message);
+}
+
 void WebGPUContext::Init()
 {
     m_WebGPUDevice = emscripten_webgpu_get_device();
+    if (!m_WebGPUDevice)
+    {
+        printf("No device!\n");
+        return;
+    }
+
+    wgpuDeviceSetUncapturedErrorCallback(m_WebGPUDevice, PrintWebGPUError, nullptr);
 
     wgpu::SurfaceDescriptorFromCanvasHTMLSelector html_surface_desc = {};
     html_surface_desc.selector = "#canvas";
