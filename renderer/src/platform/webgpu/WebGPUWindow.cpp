976909615087c1f4fd3ef34ee83a30cc5fcbad35
Willehard Korander <willehard@gmail.com>
2023-09-20 23:56:56 +0300

    Cleanup

--- renderer/src/platform/webgpu/WebGPUWindow.cpp
+++ renderer/src/platform/webgpu/WebGPUWindow.cpp
@@ -8,20 +8,22 @@ static void glfwErrorHandler(int error, const char *message)
 
 static void glfwWindowSizeCallback(GLFWwindow* window, int width, int height)
 {
-    auto *openGlWindow = (WebGPUWindow*)glfwGetWindowUserPointer(window);
-    openGlWindow->Resize({
+    std::cout << "WebGPUWindow resize" << std::endl;
+    auto *webGpuWindow = (WebGPUWindow*)glfwGetWindowUserPointer(window);
+    webGpuWindow->Resize({
         .Width = width,
         .Height = height,
-        .FramebufferWidth = static_cast<int>(openGlWindow->GetFramebufferWidth()),
-        .FramebufferHeight = static_cast<int>(openGlWindow->GetFramebufferHeight())});
+        .FramebufferWidth = static_cast<int>(webGpuWindow->GetFramebufferWidth()),
+        .FramebufferHeight = static_cast<int>(webGpuWindow->GetFramebufferHeight())});
 }
 
 static void glfwFramebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
-    auto *openGlWindow = (WebGPUWindow *) glfwGetWindowUserPointer(window);
-    openGlWindow->Resize({
-        .Width = static_cast<int>(openGlWindow->GetWidth()),
-        .Height = static_cast<int>(openGlWindow->GetHeight()),
+    printf("glfwFramebufferSizeCallback\n");
+    auto *webGpuWindow = (WebGPUWindow *) glfwGetWindowUserPointer(window);
+    webGpuWindow->Resize({
+        .Width = static_cast<int>(webGpuWindow->GetWidth()),
+        .Height = static_cast<int>(webGpuWindow->GetHeight()),
         .FramebufferWidth = width,
         .FramebufferHeight = height});
 }
@@ -37,13 +39,7 @@ WebGPUWindow::WebGPUWindow(const std::string &title, const uint32_t width, const
     glfwSetErrorCallback(glfwErrorHandler);
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-//    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
     m_Window = glfwCreateWindow(width, height, "Figment - WebGPU", nullptr, nullptr);
 
@@ -59,7 +55,6 @@ WebGPUWindow::WebGPUWindow(const std::string &title, const uint32_t width, const
     m_GfxContext = std::make_shared<WebGPUContext>();
     m_GfxContext->Init(m_Width, m_Height);
 
-    glfwShowWindow(m_Window);
 
     int w, h;
     glfwGetWindowSize(m_Window, &w, &h);
@@ -72,8 +67,8 @@ WebGPUWindow::WebGPUWindow(const std::string &title, const uint32_t width, const
     m_FramebufferHeight = (uint32_t)fh;
 
     // glfwSetWindowSizeCallback(m_Window, glfwWindowSizeCallback);
-    //    glfwSetFramebufferSizeCallback(m_Window, glfwFramebufferSizeCallback);
-    //    glfwSetWindowUserPointer(m_Window, this);
+    glfwSetFramebufferSizeCallback(m_Window, glfwFramebufferSizeCallback);
+    glfwSetWindowUserPointer(m_Window, this);
 }
 
 WebGPUWindow::~WebGPUWindow()
