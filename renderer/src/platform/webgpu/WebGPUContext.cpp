88a1cde83545050c160aaac92c415cedadf6e67a
Willehard Korander <willehard@gmail.com>
2023-09-20 15:28:26 +0300

    Test browser debugging

--- renderer/src/platform/webgpu/WebGPUContext.cpp
+++ renderer/src/platform/webgpu/WebGPUContext.cpp
@@ -8,16 +8,25 @@
 #include <vector>
 #include <iostream>
 
-static void PrintWebGPUError(WGPUErrorType error_type, const char* message, void*)
+static void PrintWebGPUError(WGPUErrorType error_type, const char *message, void *)
 {
-    const char* error_type_lbl = "";
+    const char *error_type_lbl = "";
     switch (error_type)
     {
-        case WGPUErrorType_Validation:  error_type_lbl = "Validation"; break;
-        case WGPUErrorType_OutOfMemory: error_type_lbl = "Out of memory"; break;
-        case WGPUErrorType_Unknown:     error_type_lbl = "Unknown"; break;
-        case WGPUErrorType_DeviceLost:  error_type_lbl = "Device lost"; break;
-        default:                        error_type_lbl = "Unknown";
+    case WGPUErrorType_Validation:
+        error_type_lbl = "Validation";
+        break;
+    case WGPUErrorType_OutOfMemory:
+        error_type_lbl = "Out of memory";
+        break;
+    case WGPUErrorType_Unknown:
+        error_type_lbl = "Unknown";
+        break;
+    case WGPUErrorType_DeviceLost:
+        error_type_lbl = "Device lost";
+        break;
+    default:
+        error_type_lbl = "Unknown";
     }
     printf("%s error: %s\n", error_type_lbl, message);
 }
@@ -42,7 +51,8 @@ void WebGPUContext::Init(uint32_t width, uint32_t height)
     features.resize(count);
     wgpuDeviceEnumerateFeatures(m_WebGPUDevice, features.data());
     std::cout << "Device features:" << std::endl;
-    for (auto f : features) {
+    for (auto f : features)
+    {
         std::cout << " - " << f << std::endl;
     }
 
@@ -50,10 +60,10 @@ void WebGPUContext::Init(uint32_t width, uint32_t height)
 
     WGPUSurfaceDescriptorFromCanvasHTMLSelector htmlSurfaceDesc = {};
     htmlSurfaceDesc.selector = "#canvas";
-    htmlSurfaceDesc.chain.sType =  WGPUSType_SurfaceDescriptorFromCanvasHTMLSelector;
+    htmlSurfaceDesc.chain.sType = WGPUSType_SurfaceDescriptorFromCanvasHTMLSelector;
 
     WGPUSurfaceDescriptor surfaceDesc = {};
-    surfaceDesc.nextInChain = (WGPUChainedStruct*)&htmlSurfaceDesc;
+    surfaceDesc.nextInChain = (WGPUChainedStruct *) &htmlSurfaceDesc;
 
     WGPUInstanceDescriptor instance_desc = {};
     m_Instance = wgpuCreateInstance(&instance_desc);
@@ -77,7 +87,7 @@ void WebGPUContext::Init(uint32_t width, uint32_t height)
     WGPUSwapChainDescriptor swapChainDesc = {};
     swapChainDesc.usage = WGPUTextureUsage_RenderAttachment;
     swapChainDesc.format = m_TextureFormat;
-    swapChainDesc.width =  m_SwapChainWidth;
+    swapChainDesc.width = m_SwapChainWidth;
     swapChainDesc.height = m_SwapChainHeight;
     swapChainDesc.presentMode = WGPUPresentMode_Fifo;
     m_SwapChain = wgpuDeviceCreateSwapChain(m_WebGPUDevice, m_WebGPUSurface, &swapChainDesc);
