495ff86c47eec50584f41b2adda53215dbbedf5b
Willehard Korander <willehard@gmail.com>
2023-10-26 22:31:46 +0300

    Shader compilation info needs changes to library_webgpu.js

--- renderer/src/platform/webgpu/WebGPUShader.cpp
+++ renderer/src/platform/webgpu/WebGPUShader.cpp
@@ -1,5 +1,22 @@
 #include "WebGPUShader.h"
 
+static void CompilationInfoCallback(WGPUCompilationInfoRequestStatus status, const WGPUCompilationInfo *compilationInfo, void *userdata)
+{
+    printf("WebGPUShader compilation info: status %d, message oount: %zu\n", status, compilationInfo->messageCount);
+    if (status == WGPUCompilationInfoRequestStatus_Success)
+    {
+        for (uint32_t i = 0; i < compilationInfo->messageCount; ++i)
+        {
+            const WGPUCompilationMessage &message = compilationInfo->messages[i];
+            printf("WebGPUShader compilation info: %s\n", message.message);
+        }
+    }
+    else
+    {
+        printf("WebGPUShader compilation info: failed to get compilation info\n");
+    }
+}
+
 WebGPUShader::WebGPUShader(WGPUDevice device, const std::string &shaderSource, const char *label) : m_ShaderSource(shaderSource)
 {
     WGPUShaderModuleWGSLDescriptor shaderCodeDesc = {};
@@ -13,6 +30,8 @@ WebGPUShader::WebGPUShader(WGPUDevice device, const std::string &shaderSource, c
     m_ShaderModule = wgpuDeviceCreateShaderModule(device, &shaderDesc);
 
     wgpuShaderModuleSetLabel(m_ShaderModule, label);
+
+    // wgpuShaderModuleGetCompilationInfo(m_ShaderModule, CompilationInfoCallback, nullptr);
 }
 
 WebGPUShader::~WebGPUShader()
