4b1d6a30bcefc3e159902a126d5a1582d4ffb9ab
Willehard Korander <willehard@gmail.com>
2023-11-17 10:10:17 +0200

    More static methods to submit commands

--- renderer/src/platform/webgpu/WebGPUCommand.h
+++ renderer/src/platform/webgpu/WebGPUCommand.h
@@ -11,22 +11,55 @@ public:
     template<typename T>
     static void CopyBufferToBuffer(WGPUDevice device, WebGPUBuffer<T> &from, WebGPUBuffer<T> &to, uint64_t size)
     {
-        WGPUCommandEncoderDescriptor commandEncoderDesc = {};
-        commandEncoderDesc.nextInChain = nullptr;
-        commandEncoderDesc.label = "CopyCommandEncoder";
-        WGPUCommandEncoder commandEncoder = wgpuDeviceCreateCommandEncoder(device, &commandEncoderDesc);
-
+        auto commandEncoder = CreateCommandEncoder(device, "CopyCommandEncoder");
         wgpuCommandEncoderCopyBufferToBuffer(commandEncoder, from.GetBuffer(), 0, to.GetBuffer(), 0, size);
+        auto commandBuffer = CreateCommandBuffer(device, commandEncoder, "CopyCommandBuffer");
+        SubmitCommandBuffer(device, commandBuffer);
+        DestroyCommandEncoder(commandEncoder);
+        DestroyCommandBuffer(commandBuffer);
+    }
 
-        WGPUCommandBufferDescriptor commandBufferDesc = {};
-        commandBufferDesc.nextInChain = nullptr;
-        commandBufferDesc.label = "CopyCommandBuffer";
-
-        auto commandBuffer = wgpuCommandEncoderFinish(commandEncoder, &commandBufferDesc);
+    template<typename T>
+    static void ClearBuffer(WGPUDevice device, WebGPUBuffer<T> &buffer)
+    {
+        auto commandEncoder = CreateCommandEncoder(device, "ClearBufferCommandEncoder");
+        wgpuCommandEncoderClearBuffer(commandEncoder, buffer.GetBuffer(), 0, buffer.GetSize());
+        auto commandBuffer = CreateCommandBuffer(device, commandEncoder, "ClearBufferCommandBuffer");
+        SubmitCommandBuffer(device, commandBuffer);
+        DestroyCommandEncoder(commandEncoder);
+        DestroyCommandBuffer(commandBuffer);
+    }
+private:
+    static void SubmitCommandBuffer(WGPUDevice device, WGPUCommandBuffer commandBuffer)
+    {
         WGPUQueue queue = wgpuDeviceGetQueue(device);
         wgpuQueueSubmit(queue, 1, &commandBuffer);
+    }
 
+    static WGPUCommandEncoder CreateCommandEncoder(WGPUDevice device, const char *label = "CommandEncoder")
+    {
+        WGPUCommandEncoderDescriptor commandEncoderDesc = {};
+        commandEncoderDesc.nextInChain = nullptr;
+        commandEncoderDesc.label = label;
+        return wgpuDeviceCreateCommandEncoder(device, &commandEncoderDesc);
+    }
+
+    static WGPUCommandBuffer CreateCommandBuffer(WGPUDevice device, WGPUCommandEncoder commandEncoder, const char *label = "CommandBuffer")
+    {
+        WGPUCommandBufferDescriptor commandBufferDesc = {};
+        commandBufferDesc.nextInChain = nullptr;
+        commandBufferDesc.label = label;
+        return wgpuCommandEncoderFinish(commandEncoder, &commandBufferDesc);
+    }
+
+    static void DestroyCommandEncoder(WGPUCommandEncoder commandEncoder)
+    {
         wgpuCommandEncoderRelease(commandEncoder);
+    }
+
+    static void DestroyCommandBuffer(WGPUCommandBuffer commandBuffer)
+    {
+        wgpuCommandBufferRelease(commandBuffer);
     }
 };
 
