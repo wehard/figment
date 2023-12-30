a613241c40ac36b24453a4a89a2c5cb54e134c4c
Willehard Korander <willehard@gmail.com>
2023-12-30 21:43:08 +0200

    Remove example test from editor

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -7,47 +7,6 @@
 #include "WebGPUWindow.h"
 #include "Input.h"
 
-class MatrixDisplay : public Figment::Layer
-{
-public:
-    MatrixDisplay()
-    {
-        const auto window = std::dynamic_pointer_cast<Figment::WebGPUWindow>(Figment::App::Instance()->GetWindow());
-        auto context = window->GetContext();
-        m_Scene = Figment::CreateUniquePtr<Figment::Scene>(window->GetFramebufferWidth(), window->GetFramebufferHeight());
-
-
-        for (int y = 0; y < m_Height; ++y)
-        {
-            for (int x = 0; x < m_Width; ++x)
-            {
-                auto entity = m_Scene->CreateEntity("MatrixDisplay");
-                auto &c = entity.AddComponent<Figment::CircleComponent>();
-                c.Thickness = 1;
-                auto &t = entity.GetComponent<Figment::TransformComponent>();
-                t.Position = glm::vec3(x - 32, y - 16, 0);
-            }
-        }
-    }
-
-    void OnAttach() override {}
-    void OnDetach() override {}
-    void OnUpdate(float deltaTime) override {
-        auto m_Window = Figment::App::Instance()->GetWindow();
-        m_Scene->OnUpdate(deltaTime, Figment::Input::GetMousePosition(), glm::vec2(m_Window->GetWidth(), m_Window->GetHeight()));
-    }
-    void OnImGuiRender() override {
-        ImGui::Begin("Matrix");
-        ImGui::Text("Hello, world!");
-        ImGui::End();
-    }
-    void OnEvent(Figment::AppEvent event, void *eventData) override {}
-private:
-    uint32_t m_Width = 64;
-    uint32_t m_Height = 32;
-    Figment::UniquePtr<Figment::Scene> m_Scene;
-};
-
 Figment::App *app;
 
 static void main_loop(void *arg)
@@ -67,7 +26,6 @@ int main(int argc, char **argv)
 	printf("%s:%d Initial canvas size %u x %u\n", __FILE__, __LINE__, width, height);
 	app = new Figment::App(width, height);
     app->AddLayer(Figment::CreateUniquePtr<Figment::EditorLayer>());
-    // app->AddLayer(Figment::CreateUniquePtr<MatrixDisplay>());
 
 	emscripten_set_main_loop_arg(main_loop, app, 0, false);
 }
