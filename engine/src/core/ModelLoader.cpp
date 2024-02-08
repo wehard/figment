b648d5ecb33e84a519bdb60ee8b3700b96ade2d2
Willehard Korander <willehard@gmail.com>
2024-02-08 13:46:21 +0200

    Load obj

--- engine/src/core/ModelLoader.cpp
+++ engine/src/core/ModelLoader.cpp
@@ -5,6 +5,9 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "tiny_gltf.h"
 
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
 namespace Figment::ModelLoader
 {
     tinygltf::Model LoadGltf(const std::string &path)
@@ -31,5 +34,30 @@ namespace Figment::ModelLoader
             FIG_LOG_ERROR("Failed to parse glTF");
         }
         return model;
+    }
+
+    MeshData LoadObj(const std::string &path)
+    {
+        tinyobj::ObjReader reader;
+        tinyobj::ObjReaderConfig readerConfig;
+        readerConfig.triangulate = false;
+
+        if (!reader.ParseFromFile(path, readerConfig))
+        {
+            FIG_LOG_ERROR("Failed to parse .obj file %s", path.c_str());
+            return {};
+        }
+        MeshData meshData;
+        auto &attrib = reader.GetAttrib();
+        auto &shapes = reader.GetShapes();
+        meshData.Vertices = attrib.GetVertices();
+        auto shape = shapes[0];
+        for (const auto &index : shape.mesh.indices)
+        {
+            meshData.Indices.push_back(index.vertex_index);
+        }
+        FIG_LOG_INFO("Loaded .obj file %s, VertexCount = %d, IndexCount = %d", path.c_str(), meshData.VertexCount(),
+                meshData.IndexCount());
+        return meshData;
     }
 }
--- engine/src/core/ModelLoader.h
+++ engine/src/core/ModelLoader.h
@@ -1,9 +1,27 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include "tiny_gltf.h"
 
 namespace Figment::ModelLoader
 {
+    struct MeshData
+    {
+        std::vector<float> Vertices;
+        std::vector<uint32_t> Indices;
+
+        uint32_t VertexCount() const
+        {
+            return Vertices.size();
+        }
+
+        uint32_t IndexCount() const
+        {
+            return Indices.size();
+        }
+    };
+
     tinygltf::Model LoadGltf(const std::string &path);
+    MeshData LoadObj(const std::string &path);
 }
