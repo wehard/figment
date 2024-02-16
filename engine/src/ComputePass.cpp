80d60e135e187235e3463131a09cc5e35161e111
Willehard Korander <willehard@gmail.com>
2024-02-16 19:50:34 +0200

    Remove old api

--- engine/src/ComputePass.cpp
+++ engine/src/ComputePass.cpp
@@ -3,12 +3,6 @@
 
 namespace Figment
 {
-
-    ComputePass::ComputePass(WGPUDevice device, WebGPUShader *shader) :
-            m_Device(device), m_Shader(shader)
-    {
-    }
-
     ComputePass::ComputePass(WGPUDevice device, ComputePipeline *pipeline, BindGroup *bindGroup) :
             m_Device(device), m_Pipeline(pipeline), m_BindGroup(bindGroup)
     {
@@ -47,68 +41,5 @@ namespace Figment
 
         m_CommandEncoder = nullptr;
         m_ComputePassEncoder = nullptr;
-    }
-
-    void ComputePass::Bind(WGPUBuffer buffer, uint32_t size, WGPUBufferBindingType type)
-    {
-        WGPUBindGroupEntry bindGroupEntry = {};
-        bindGroupEntry.binding = m_BindGroupEntries.size();
-        bindGroupEntry.buffer = buffer;
-        bindGroupEntry.offset = 0;
-        bindGroupEntry.size = size;
-
-        m_BindGroupEntries.emplace_back(bindGroupEntry);
-
-        WGPUBindGroupLayoutEntry bindGroupLayoutEntry = GetDefaultWGPUBindGroupLayoutEntry();
-        bindGroupLayoutEntry.nextInChain = nullptr;
-        bindGroupLayoutEntry.binding = bindGroupEntry.binding;
-        bindGroupLayoutEntry.visibility = WGPUShaderStage_Compute;
-        bindGroupLayoutEntry.buffer.type = type;
-
-        m_BindGroupLayoutEntries.emplace_back(bindGroupLayoutEntry);
-    }
-
-    void ComputePass::BindTexture(WGPUTextureView textureView, uint64_t size)
-    {
-        WGPUBindGroupEntry bindGroupEntry = {};
-        bindGroupEntry.size = size;
-        bindGroupEntry.offset = 0;
-        bindGroupEntry.binding = m_BindGroupEntries.size();
-        bindGroupEntry.textureView = textureView;
-        bindGroupEntry.buffer = nullptr;
-        bindGroupEntry.sampler = nullptr;
-
-        m_BindGroupEntries.emplace_back(bindGroupEntry);
-
-        WGPUBindGroupLayoutEntry bindGroupLayoutEntry = GetDefaultWGPUBindGroupLayoutEntry();
-        bindGroupLayoutEntry.nextInChain = nullptr;
-        bindGroupLayoutEntry.binding = bindGroupEntry.binding;
-        bindGroupLayoutEntry.visibility = WGPUShaderStage_Compute;
-        bindGroupLayoutEntry.texture.sampleType = WGPUTextureSampleType_Float;
-        bindGroupLayoutEntry.texture.viewDimension = WGPUTextureViewDimension_2D;
-        bindGroupLayoutEntry.texture.multisampled = false;
-
-        m_BindGroupLayoutEntries.emplace_back(bindGroupLayoutEntry);
-    }
-
-    void ComputePass::BindSampler(WGPUSampler sampler)
-    {
-        WGPUBindGroupEntry bindGroupEntry = GetDefaultWGPUBindGroupEntry();
-        bindGroupEntry.binding = m_BindGroupEntries.size();
-        bindGroupEntry.sampler = sampler;
-
-        m_BindGroupEntries.emplace_back(bindGroupEntry);
-
-        WGPUSamplerBindingLayout samplerBindingLayout = {};
-        samplerBindingLayout.nextInChain = nullptr;
-        samplerBindingLayout.type = WGPUSamplerBindingType_Filtering;
-
-        WGPUBindGroupLayoutEntry bindGroupLayoutEntry = GetDefaultWGPUBindGroupLayoutEntry();
-        bindGroupLayoutEntry.nextInChain = nullptr;
-        bindGroupLayoutEntry.binding = bindGroupEntry.binding;
-        bindGroupLayoutEntry.visibility = WGPUShaderStage_Compute;
-        bindGroupLayoutEntry.sampler = samplerBindingLayout;
-
-        m_BindGroupLayoutEntries.emplace_back(bindGroupLayoutEntry);
     }
 }
--- engine/src/ComputePass.h
+++ engine/src/ComputePass.h
@@ -1,10 +1,8 @@
 #pragma once
 
 #include "WebGPUBuffer.h"
-#include "WebGPUShader.h"
-#include "WebGPUTexture.h"
-#include "webgpu/webgpu.h"
 #include "ComputePipeline.h"
+#include "webgpu/webgpu.h"
 #include <vector>
 
 namespace Figment
@@ -12,38 +10,16 @@ namespace Figment
     class ComputePass
     {
     public:
-        ComputePass(WGPUDevice device, WebGPUShader *shader);
         ComputePass(WGPUDevice device, ComputePipeline *pipeline, BindGroup *bindGroup);
         ~ComputePass() = default;
         void Begin();
         void Dispatch(const std::string &name, uint32_t invocationsX);
         void End();
-        template<typename T>
-        void Bind(WebGPUVertexBuffer<T> &buffer)
-        {
-            Bind(buffer.GetBuffer(), buffer.GetSize(), WGPUBufferBindingType_Storage);
-        }
-        template<typename T>
-        void Bind(WebGPUUniformBuffer<T> &buffer)
-        {
-            Bind(buffer.GetBuffer(), buffer.GetSize(), WGPUBufferBindingType_Uniform);
-        }
-        void Bind(WebGPUTexture &texture)
-        {
-            BindTexture(texture.GetTextureView(), texture.GetWidth() * texture.GetHeight() * 4);
-            BindSampler(texture.GetSampler());
-        }
     private:
-        void Bind(WGPUBuffer buffer, uint32_t size, WGPUBufferBindingType type);
-        void BindTexture(WGPUTextureView textureView, uint64_t size);
-        void BindSampler(WGPUSampler sampler);
-        std::vector<WGPUBindGroupEntry> m_BindGroupEntries;
-        std::vector<WGPUBindGroupLayoutEntry> m_BindGroupLayoutEntries;
-        WGPUDevice m_Device;
-        WebGPUShader *m_Shader;
-        WGPUCommandEncoder m_CommandEncoder;
-        WGPUComputePassEncoder m_ComputePassEncoder;
-        ComputePipeline *m_Pipeline;
-        BindGroup *m_BindGroup;
+        WGPUDevice m_Device = nullptr;
+        WGPUCommandEncoder m_CommandEncoder = nullptr;
+        WGPUComputePassEncoder m_ComputePassEncoder = nullptr;
+        ComputePipeline *m_Pipeline = nullptr;
+        BindGroup *m_BindGroup = nullptr;
     };
 }
