e77eb737fa567b1bf3c909861e914efc3e7f9346
Willehard Korander <willehard@gmail.com>
2024-02-16 19:38:03 +0200

    Remove old code

--- engine/src/ComputePipeline.cpp
+++ engine/src/ComputePipeline.cpp
@@ -15,13 +15,6 @@ namespace Figment
 
     void ComputePipeline::Build(const char *entryPoint, WGPUShaderModule shaderModule)
     {
-        // WGPUBindGroupLayoutDescriptor bindGroupLayoutDesc = {};
-        // bindGroupLayoutDesc.label = "ComputeBindGroupLayout";
-        // bindGroupLayoutDesc.entryCount = m_BindGroupLayoutEntries.size();
-        // bindGroupLayoutDesc.entries = m_BindGroupLayoutEntries.data();
-
-        // auto bindGroupLayout = wgpuDeviceCreateBindGroupLayout(m_Device, &bindGroupLayoutDesc);
-
         WGPUPipelineLayoutDescriptor pipelineLayoutDesc = {};
         pipelineLayoutDesc.label = "ComputePipelineLayout";
         pipelineLayoutDesc.bindGroupLayoutCount = 1;
@@ -34,75 +27,5 @@ namespace Figment
         computePipelineDesc.compute.module = shaderModule;
         computePipelineDesc.layout = pipelineLayout;
         Pipeline = wgpuDeviceCreateComputePipeline(m_Device, &computePipelineDesc);
-
-        // WGPUBindGroupDescriptor bindGroupDesc = {};
-        // bindGroupDesc.label = "ComputeBindGroup";
-        // bindGroupDesc.layout = bindGroupLayout;
-        // bindGroupDesc.entryCount = m_BindGroupEntries.size();
-        // bindGroupDesc.entries = m_BindGroupEntries.data();
-        // BindGroup = wgpuDeviceCreateBindGroup(m_Device, &bindGroupDesc);
     }
-
-    // void ComputePipeline::Bind(WGPUBuffer buffer, uint32_t size, WGPUBufferBindingType type)
-    // {
-    //     WGPUBindGroupEntry bindGroupEntry = {};
-    //     bindGroupEntry.binding = m_BindGroupEntries.size();
-    //     bindGroupEntry.buffer = buffer;
-    //     bindGroupEntry.offset = 0;
-    //     bindGroupEntry.size = size;
-    //
-    //     m_BindGroupEntries.emplace_back(bindGroupEntry);
-    //
-    //     WGPUBindGroupLayoutEntry bindGroupLayoutEntry = GetDefaultWGPUBindGroupLayoutEntry();
-    //     bindGroupLayoutEntry.nextInChain = nullptr;
-    //     bindGroupLayoutEntry.binding = bindGroupEntry.binding;
-    //     bindGroupLayoutEntry.visibility = WGPUShaderStage_Compute;
-    //     bindGroupLayoutEntry.buffer.type = type;
-    //
-    //     m_BindGroupLayoutEntries.emplace_back(bindGroupLayoutEntry);
-    // }
-
-    // void ComputePipeline::BindTexture(WGPUTextureView textureView, uint64_t size)
-    // {
-    //     WGPUBindGroupEntry bindGroupEntry = {};
-    //     bindGroupEntry.size = size;
-    //     bindGroupEntry.offset = 0;
-    //     bindGroupEntry.binding = m_BindGroupEntries.size();
-    //     bindGroupEntry.textureView = textureView;
-    //     bindGroupEntry.buffer = nullptr;
-    //     bindGroupEntry.sampler = nullptr;
-    //
-    //     m_BindGroupEntries.emplace_back(bindGroupEntry);
-    //
-    //     WGPUBindGroupLayoutEntry bindGroupLayoutEntry = GetDefaultWGPUBindGroupLayoutEntry();
-    //     bindGroupLayoutEntry.nextInChain = nullptr;
-    //     bindGroupLayoutEntry.binding = bindGroupEntry.binding;
-    //     bindGroupLayoutEntry.visibility = WGPUShaderStage_Compute;
-    //     bindGroupLayoutEntry.texture.sampleType = WGPUTextureSampleType_Float;
-    //     bindGroupLayoutEntry.texture.viewDimension = WGPUTextureViewDimension_2D;
-    //     bindGroupLayoutEntry.texture.multisampled = false;
-    //
-    //     m_BindGroupLayoutEntries.emplace_back(bindGroupLayoutEntry);
-    // }
-
-    // void ComputePipeline::BindSampler(WGPUSampler sampler)
-    // {
-    //     WGPUBindGroupEntry bindGroupEntry = GetDefaultWGPUBindGroupEntry();
-    //     bindGroupEntry.binding = m_BindGroupEntries.size();
-    //     bindGroupEntry.sampler = sampler;
-    //
-    //     m_BindGroupEntries.emplace_back(bindGroupEntry);
-    //
-    //     WGPUSamplerBindingLayout samplerBindingLayout = {};
-    //     samplerBindingLayout.nextInChain = nullptr;
-    //     samplerBindingLayout.type = WGPUSamplerBindingType_Filtering;
-    //
-    //     WGPUBindGroupLayoutEntry bindGroupLayoutEntry = GetDefaultWGPUBindGroupLayoutEntry();
-    //     bindGroupLayoutEntry.nextInChain = nullptr;
-    //     bindGroupLayoutEntry.binding = bindGroupEntry.binding;
-    //     bindGroupLayoutEntry.visibility = WGPUShaderStage_Compute;
-    //     bindGroupLayoutEntry.sampler = samplerBindingLayout;
-    //
-    //     m_BindGroupLayoutEntries.emplace_back(bindGroupLayoutEntry);
-    // }
 }
--- engine/src/ComputePipeline.h
+++ engine/src/ComputePipeline.h
@@ -15,32 +15,9 @@ namespace Figment
         void Build(const char *entryPoint, WGPUShaderModule shaderModule);
         ~ComputePipeline();
 
-        // template<typename T>
-        // void Bind(WebGPUVertexBuffer<T> &buffer)
-        // {
-        //     Bind(buffer.GetBuffer(), buffer.GetSize(), WGPUBufferBindingType_Storage);
-        // }
-        // template<typename T>
-        // void Bind(WebGPUUniformBuffer<T> &buffer)
-        // {
-        //     Bind(buffer.GetBuffer(), buffer.GetSize(), WGPUBufferBindingType_Uniform);
-        // }
-        // void Bind(WebGPUTexture &texture)
-        // {
-        //     BindTexture(texture.GetTextureView(), texture.GetWidth() * texture.GetHeight() * 4);
-        //     BindSampler(texture.GetSampler());
-        // }
-
         WGPUComputePipeline Pipeline;
-        // WGPUBindGroup BindGroup;
     private:
-        // void Bind(WGPUBuffer buffer, uint32_t size, WGPUBufferBindingType type);
-        // void BindTexture(WGPUTextureView textureView, uint64_t size);
-        // void BindSampler(WGPUSampler sampler);
-
         WGPUDevice m_Device;
         BindGroup &m_BindGroup;
-        // std::vector<WGPUBindGroupLayoutEntry> m_BindGroupLayoutEntries;
-        // std::vector<WGPUBindGroupEntry> m_BindGroupEntries;
     };
 }
