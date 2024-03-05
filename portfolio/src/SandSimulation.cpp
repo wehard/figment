abde7f3ac38617b175fcf2def99f1711c6855c92
Willehard Korander <willehard@gmail.com>
2024-03-05 09:15:34 +0200

    Sand sim updates

--- portfolio/src/SandSimulation.cpp
+++ portfolio/src/SandSimulation.cpp
@@ -1,12 +1,29 @@
 #include "SandSimulation.h"
 
 SandSimulation::SandSimulation(Figment::WebGPUContext &context, PerspectiveCamera &camera) : Layer("SandSimulation"),
-        m_Context(context), m_Camera(camera),
-        m_Canvas(context, 320, 200)
+        m_Context(context), m_Camera(camera)
 {
-    m_PixelCanvas = new PixelCanvas(context, 320, 200);
+    m_PixelCanvas = new PixelCanvas(context, s_Width, s_Height);
 
-    m_PixelCanvas->SetPixel(0, 0, 0xff00ffff);
+    m_PixelCanvas->SetPixel(0, 0, 0xff0000ff);
+    m_PixelCanvas->SetPixel(0, 255, 0x00ff00ff);
+    m_PixelCanvas->SetPixel(255, 255, 0x0000ffff);
+    m_PixelCanvas->SetPixel(255, 0, 0xffffffff);
+
+    for (auto x = 0; x < s_Width * s_Height; x++)
+    {
+        auto i = x % s_Width;
+        auto j = x / s_Width;
+        // if ((i / 10 + j / 10) % 2 == 0)
+        //     m_PixelCanvas->SetPixel(i, j, 0xff000000);
+        // else
+        //     m_PixelCanvas->SetPixel(i, j, 0xffffffff);
+        auto r = Random::Float();
+        if (r < 0.2)
+            m_PixelCanvas->SetPixel(i, j, s_SandColor);
+        else
+            m_PixelCanvas->SetPixel(i, j, s_BackgroundColor);
+    }
     m_PixelCanvas->UpdateTexture();
 }
 
@@ -20,8 +37,43 @@ void SandSimulation::OnDetach()
 
 }
 
+bool SandSimulation::CanMove(PixelCanvas &canvas, int x, int y)
+{
+    if (x < 0 || x >= canvas.GetWidth() || y < 0 || y >= canvas.GetHeight())
+        return false;
+    return canvas.GetPixel(x, y) == s_BackgroundColor;
+}
+
 void SandSimulation::OnUpdate(float deltaTime)
 {
+    auto r = Random::Float();
+    m_PixelCanvas->SetPixel(
+            ((s_Width / 2) + (uint32_t)(10 * r)) + glm::sin(App::Instance()->GetTimeSinceStart()) * 30,
+            180,
+            App::Instance()->GetTimeSinceStart() > 8.0 ? s_WaterColor : s_SandColor);
+
+    bool updated = false;
+    for (int y = 0; y < s_Height; y++)
+    {
+        for (int x = 0; x < s_Width; x++)
+        {
+            auto color = m_PixelCanvas->GetPixel(x, y);
+            switch (color)
+            {
+            case s_SandColor:
+                UpdateSand(x, y);
+                break;
+            case s_WaterColor:
+                UpdateWater(x, y);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    if (m_Dirty)
+        m_PixelCanvas->UpdateTexture();
+
     m_PixelCanvas->OnUpdate(m_Camera, deltaTime);
 }
 
@@ -33,4 +85,99 @@ void SandSimulation::OnImGuiRender()
 void SandSimulation::OnEvent(Figment::AppEvent event, void *eventData)
 {
 
+}
+
+void SandSimulation::UpdateSand(int x, int y)
+{
+    if (x < 0 || x >= m_PixelCanvas->GetWidth() || y < 0 || y >= m_PixelCanvas->GetHeight())
+        return;
+    if (CanMove(*m_PixelCanvas, x, y - 1))
+    {
+        auto below = m_PixelCanvas->GetPixel(x, y - 1);
+        m_PixelCanvas->SetPixel(x, y, below);
+        m_PixelCanvas->SetPixel(x, y - 1, s_SandColor);
+        m_Dirty = true;
+        return;
+    }
+    bool canMoveDownLeft = CanMove(*m_PixelCanvas, x - 1, y - 1);
+    bool canMoveDownRight = CanMove(*m_PixelCanvas, x + 1, y - 1);
+    if (canMoveDownLeft && canMoveDownRight)
+    {
+        auto fc = App::Instance()->GetFPSCounter().GetFrameCount() % 2;
+        fc == 0 ? canMoveDownRight = false : canMoveDownLeft = false;
+    }
+    if (canMoveDownLeft)
+    {
+        auto below = m_PixelCanvas->GetPixel(x - 1, y - 1);
+        m_PixelCanvas->SetPixel(x, y, below);
+        m_PixelCanvas->SetPixel(x - 1, y - 1, s_SandColor);
+        m_Dirty = true;
+    }
+    else if (canMoveDownRight)
+    {
+        auto below = m_PixelCanvas->GetPixel(x + 1, y - 1);
+        m_PixelCanvas->SetPixel(x, y, below);
+        m_PixelCanvas->SetPixel(x + 1, y - 1, s_SandColor);
+        m_Dirty = true;
+    }
+}
+
+void SandSimulation::UpdateWater(int x, int y)
+{
+    if (CanMove(*m_PixelCanvas, x, y - 1))
+    {
+        auto other = m_PixelCanvas->GetPixel(x, y - 1);
+        m_PixelCanvas->SetPixel(x, y, other);
+        m_PixelCanvas->SetPixel(x, y - 1, s_WaterColor);
+        m_Dirty = true;
+        return;
+    }
+
+    bool canMoveDownLeft = CanMove(*m_PixelCanvas, x - 1, y - 1);
+    bool canMoveDownRight = CanMove(*m_PixelCanvas, x + 1, y - 1);
+    if (canMoveDownLeft && canMoveDownRight)
+    {
+        auto fc = App::Instance()->GetFPSCounter().GetFrameCount() % 2;
+        fc == 0 ? canMoveDownRight = false : canMoveDownLeft = false;
+    }
+    if (canMoveDownLeft)
+    {
+        auto below = m_PixelCanvas->GetPixel(x - 1, y - 1);
+        m_PixelCanvas->SetPixel(x, y, below);
+        m_PixelCanvas->SetPixel(x - 1, y - 1, s_WaterColor);
+        m_Dirty = true;
+        return;
+    }
+    if (canMoveDownRight)
+    {
+        auto below = m_PixelCanvas->GetPixel(x + 1, y - 1);
+        m_PixelCanvas->SetPixel(x, y, below);
+        m_PixelCanvas->SetPixel(x + 1, y - 1, s_WaterColor);
+        m_Dirty = true;
+        return;
+    }
+
+    bool canMoveLeft = CanMove(*m_PixelCanvas, x - 1, y);
+    bool canMoveRight = CanMove(*m_PixelCanvas, x + 1, y);
+    if (canMoveLeft && canMoveRight)
+    {
+        auto fc = App::Instance()->GetFPSCounter().GetFrameCount() % 2;
+        fc == 0 ? canMoveRight = false : canMoveLeft = false;
+    }
+    if (canMoveLeft)
+    {
+        auto below = m_PixelCanvas->GetPixel(x - 1, y);
+        m_PixelCanvas->SetPixel(x, y, below);
+        m_PixelCanvas->SetPixel(x - 1, y, s_WaterColor);
+        m_Dirty = true;
+        return;
+    }
+    if (canMoveRight)
+    {
+        auto other = m_PixelCanvas->GetPixel(x + 1, y);
+        m_PixelCanvas->SetPixel(x, y, other);
+        m_PixelCanvas->SetPixel(x + 1, y, s_WaterColor);
+        m_Dirty = true;
+        return;
+    }
 }
--- portfolio/src/SandSimulation.h
+++ portfolio/src/SandSimulation.h
@@ -16,7 +16,17 @@ public:
     void OnEvent(Figment::AppEvent event, void *eventData) override;
 private:
     WebGPUContext &m_Context;
-    PixelCanvas m_Canvas;
     PerspectiveCamera &m_Camera;
     PixelCanvas *m_PixelCanvas;
+
+    bool CanMove(PixelCanvas &canvas, int x, int y);
+    void UpdateSand(int x, int y);
+    void UpdateWater(int x, int y);
+    bool m_Dirty = false;
+
+    constexpr static uint32_t s_Width = 320;
+    constexpr static uint32_t s_Height = 200;
+    constexpr static uint32_t s_SandColor = 0xff004b96;
+    constexpr static uint32_t s_WaterColor = 0xffff0000;
+    constexpr static uint32_t s_BackgroundColor = 0xffa89ba1;
 };
