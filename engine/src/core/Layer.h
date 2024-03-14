ae319e2e539f690611d9281cd15aee7a870000a4
Willehard Korander <willehard@gmail.com>
2024-03-14 19:30:20 +0200

    Formatting

--- engine/src/core/Layer.h
+++ engine/src/core/Layer.h
@@ -7,27 +7,27 @@ namespace Figment
     enum class AppEvent
     {
         None = 0,
-        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved
+        WindowClose,
+        WindowResize,
+        WindowFocus,
+        WindowLostFocus,
+        WindowMoved
     };
 
     class Layer
     {
     public:
         friend class App;
-        explicit Layer(const std::string &name = "Layer", bool enabled = true) : m_Name(name), m_Enabled(enabled)
-        { };
+        explicit Layer(const std::string &name = "Layer", bool enabled = true) : m_Name(name), m_Enabled(enabled) { };
         virtual ~Layer() = default;
         virtual void OnAttach() = 0;
         virtual void OnDetach() = 0;
         virtual void OnUpdate(float deltaTime) = 0;
         virtual void OnImGuiRender() = 0;
         virtual void OnEvent(AppEvent event, void *eventData) = 0;
-        void SetEnabled(bool enabled)
-        { m_Enabled = enabled; }
-        bool IsEnabled() const
-        { return m_Enabled; }
-        std::string GetName() const
-        { return m_Name; }
+        void SetEnabled(bool enabled) { m_Enabled = enabled; }
+        bool IsEnabled() const { return m_Enabled; }
+        std::string GetName() const { return m_Name; }
     protected:
         std::string m_Name;
         bool m_Enabled = true;
