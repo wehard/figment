000fc66bcebb6d29e4a4db8b01427b5af2feefce
Willehard Korander <willehard@gmail.com>
2024-05-17 09:55:46 +0300

    Hide members provide getters

--- engine/src/core/Handle.h
+++ engine/src/core/Handle.h
@@ -5,6 +5,9 @@
 namespace Figment
 {
     template<typename T>
+    class Pool;
+
+    template<typename T>
     struct Handle
     {
         explicit Handle(uint32_t index = 0, uint32_t generation = 0) : index(index), generation(generation) { }
@@ -19,6 +22,12 @@ namespace Figment
         // {
         //     return index == other.index && generation == other.generation;
         // }
+
+        [[nodiscard]] uint32_t Index() const { return index; }
+        [[nodiscard]] uint32_t Generation() const { return generation; }
+
+    private:
+        friend class Pool<T>;
 
         uint32_t index = 0;
         uint32_t generation = 0;
