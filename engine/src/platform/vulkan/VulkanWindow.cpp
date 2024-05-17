287ca7afbf2530e5fc10d295c56b747f7ed9c1a5
Willehard Korander <willehard@gmail.com>
2024-05-17 11:23:05 +0300

    Begin vk window implementation

--- engine/src/platform/vulkan/VulkanWindow.cpp
+++ engine/src/platform/vulkan/VulkanWindow.cpp
@@ -1,7 +1,30 @@
 #include "VulkanWindow.h"
+#include "Log.h"
+#include "FigmentAssert.h"
 
 Figment::VulkanWindow::VulkanWindow(const std::string &title, uint32_t width, uint32_t height) : Window(title, width,
         height)
 {
+    if (!glfwInit())
+    {
+        FIGMENT_ASSERT(false, "Failed to initialize GLFW");
+    }
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    m_Window = glfwCreateWindow((int)width, (int)height, "Figment (Vulkan)", nullptr, nullptr);
+
+    if (!m_Window)
+    {
+        FIGMENT_ASSERT(false, "Failed to create GLFW window");
+    }
+
+    glfwSetWindowUserPointer(m_Window, this);
+    // glfwSetWindowSizeCallback(m_Window, glfwWindowSizeCallback);
+    // glfwSetFramebufferSizeCallback(m_Window, glfwFramebufferSizeCallback);
+
+
+    // m_RenderContext = std::make_shared<VulkanContext>();
+    // m_RenderContext->Init(m_Width, m_Height);
 }
