4fc5c2b244dad0ac5c621b87fbf8e096320d3ce6
Willehard Korander <willehard@gmail.com>
2024-05-17 09:59:01 +0300

    Copnditional build window

--- engine/src/core/Window.cpp
+++ engine/src/core/Window.cpp
@@ -1,12 +1,19 @@
 #include "Window.h"
 #include "FigmentAssert.h"
-#include "WebGPUWindow.h"
 #include <string>
+
+#ifdef __EMSCRIPTEN__
+#include "WebGPUWindow.h"
+#endif
 
 namespace Figment
 {
     std::shared_ptr<Window> Window::Create(const std::string &title, const uint32_t width, const uint32_t height)
     {
+#ifdef __EMSCRIPTEN__
         return std::make_shared<WebGPUWindow>(title, width, height);
+#endif
+        FIGMENT_ASSERT(false, "Window creation not implemented for this platform");
+        return nullptr;
     }
 }
