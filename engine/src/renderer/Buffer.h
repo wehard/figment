12289b6e9aea5713bc6fc4dbfbeedd8c31359303
Willehard Korander <willehard@gmail.com>
2024-05-17 09:32:54 +0300

    Add buffer

---
+++ engine/src/renderer/Buffer.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Figment
+{
+    enum class BufferUsage
+    {
+        None = 0x00000000,
+        MapRead = 0x00000001,
+        MapWrite = 0x00000002,
+        CopySrc = 0x00000004,
+        CopyDst = 0x00000008,
+        Index = 0x00000010,
+        Vertex = 0x00000020,
+        Uniform = 0x00000040,
+        Storage = 0x00000080,
+    };
+
+    struct BufferDescriptor
+    {
+        uint32_t ByteSize = 0;
+        BufferUsage Usage = BufferUsage::None;
+    };
+
+    class Buffer
+    {
+    public:
+        Buffer() = default;
+        explicit Buffer(const BufferDescriptor &&descriptor)
+        {
+            m_ByteSize = descriptor.ByteSize;
+        };
+        ~Buffer() = default;
+
+        [[nodiscard]] uint32_t GetByteSize() const { return m_ByteSize; }
+
+        static Buffer *Create(uint32_t sizeBytes, BufferUsage usage)
+        {
+            return new Buffer({ .ByteSize = sizeBytes, .Usage = usage });
+        }
+
+    private:
+        uint32_t m_ByteSize = 0;
+    };
+}
