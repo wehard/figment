a0d73c9084848f9e778df933192b7bfc1d5c18e4
Willehard Korander <willehard@gmail.com>
2024-05-20 21:12:54 +0300

    Poll events to display glfw window

--- vk-app/src/main.cpp
+++ vk-app/src/main.cpp
@@ -7,54 +7,96 @@
 
 using namespace Figment;
 
+static VkRenderPass CreateImGuiRenderPass(VulkanContext *vkContext)
+{
+    VkAttachmentDescription attachmentDescription = {};
+    attachmentDescription.format = vkContext->SurfaceDetails().formats[0].format;
+    attachmentDescription.samples = VK_SAMPLE_COUNT_1_BIT;
+    attachmentDescription.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
+    attachmentDescription.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    attachmentDescription.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    attachmentDescription.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    attachmentDescription.initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+    attachmentDescription.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference colorAttachment = {};
+    colorAttachment.attachment = 0;
+    colorAttachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachment;
+
+    VkSubpassDependency subpassDependency = {};
+    subpassDependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+    subpassDependency.dstSubpass = 0;
+    subpassDependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    subpassDependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    subpassDependency.srcAccessMask = 0;  // or VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+    subpassDependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
+    VkRenderPassCreateInfo info = {};
+    info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    info.attachmentCount = 1;
+    info.pAttachments = &attachmentDescription;
+    info.subpassCount = 1;
+    info.pSubpasses = &subpass;
+    info.dependencyCount = 1;
+    info.pDependencies = &subpassDependency;
+    VkRenderPass renderPass;
+    if (vkCreateRenderPass(vkContext->GetDevice(), &info, nullptr, &renderPass) != VK_SUCCESS) {
+        throw std::runtime_error("Could not create Dear ImGui's render pass");
+    }
+    return renderPass;
+}
+
 int main()
 {
     Log::Init();
     auto window = Figment::Window::Create("Figment", 1280, 720);
-
-    auto context = ImGui::CreateContext();
-    ImGui_ImplGlfw_InitForVulkan((GLFWwindow*)window->GetNative(), true);
     auto vkContext = window->GetContext<VulkanContext>();
-    ImGui_ImplVulkan_InitInfo initInfo = {};
-    initInfo.UseDynamicRendering = true;
-    VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo = {};
-    pipelineRenderingCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
 
-    initInfo.PipelineRenderingCreateInfo = pipelineRenderingCreateInfo;
-    initInfo.Device = vkContext->GetDevice();
-    initInfo.Instance = vkContext->GetInstance();
-    initInfo.PhysicalDevice = vkContext->GetPhysicalDevice();
-    initInfo.Queue = vkContext->GetGraphicsQueue();
-    // initInfo.QueueFamily = g_QueueFamily;
-    // initInfo.PipelineCache = g_PipelineCache;
-    initInfo.DescriptorPool = vkContext->GetDescriptorPool();
-    initInfo.RenderPass = vkContext->GetRenderPass();
-    initInfo.Subpass = 0;
-    initInfo.MinImageCount = vkContext->SurfaceDetails().surfaceCapabilities.minImageCount;
-    initInfo.ImageCount = vkContext->SurfaceDetails().surfaceCapabilities.minImageCount + 1;
+    // auto renderPass = CreateImGuiRenderPass(vkContext.get());
+
+    // auto context = ImGui::CreateContext();
+    // ImGui_ImplGlfw_InitForVulkan((GLFWwindow*)window->GetNative(), true);
+    // ImGui_ImplVulkan_InitInfo initInfo = {};
+    // initInfo.UseDynamicRendering = false;
+    // initInfo.Device = vkContext->GetDevice();
+    // initInfo.Instance = vkContext->GetInstance();
+    // initInfo.PhysicalDevice = vkContext->GetPhysicalDevice();
+    // initInfo.Queue = vkContext->GetGraphicsQueue();
+    // initInfo.QueueFamily = 0;
+    // initInfo.PipelineCache = VK_NULL_HANDLE;
+    // initInfo.DescriptorPool = vkContext->GetDescriptorPool();
+    // initInfo.RenderPass = renderPass;
+    // initInfo.Subpass = 0;
+    // initInfo.MinImageCount = vkContext->SurfaceDetails().surfaceCapabilities.minImageCount;
+    // initInfo.ImageCount = vkContext->SurfaceDetails().surfaceCapabilities.minImageCount + 1;
     // initInfo.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
     // initInfo.Allocator = nullptr;
-    // initInfo.CheckVkResultFn = check_vk_result;
-    ImGui_ImplVulkan_Init(&initInfo);
+    // initInfo.CheckVkResultFn = nullptr;
+    // ImGui_ImplVulkan_Init(&initInfo);
+    // ImGui_ImplVulkan_CreateFontsTexture();
 
     while (!window->ShouldClose())
     {
-        ImGui_ImplGlfw_NewFrame();
-        ImGui_ImplVulkan_NewFrame();
-        ImGui::NewFrame();
-        ImGui::Begin("Hello, world!");
-        ImGui::Text("This is some useful text.");
-        ImGui::End();
-        ImGui::Render();
-        ImGui::EndFrame();
+        glfwPollEvents();
+        // ImGui_ImplGlfw_NewFrame();
+        // ImGui_ImplVulkan_NewFrame();
+        // ImGui::NewFrame();
+        // ImGui::Begin("Hello, world!");
+        // ImGui::Text("This is some useful text.");
+        // ImGui::End();
+        // ImGui::Render();
+        // ImGui::EndFrame();
 
-        vkContext->BeginFrame();
-        ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), vkContext->GetCommandBuffer(), vkContext->GetPipeline());
-        vkContext->EndFrame();
+        vkContext->DebugDraw();
     }
 
     ImGui_ImplVulkan_Shutdown();
     ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext(context);
+    // ImGui::DestroyContext(context);
     return 0;
 }
