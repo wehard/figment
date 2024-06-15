0d5c2119eb112e04c84f15632c9639e7d91d07e0
Willehard Korander <willehard@gmail.com>
2024-06-15 08:00:00 +0300

    Begin arena allocator

---
+++ engine/src/core/Arena.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdint>
+
+struct Arena
+{
+public:
+    explicit Arena(uint32_t size) : m_Size(size)
+    {
+        m_Base = (char *)malloc(size);
+        m_Pointer = m_Base;
+        bzero(m_Base, size);
+    }
+
+    ~Arena()
+    {
+        free(m_Base);
+    }
+
+    template<typename T>
+    T *Allocate()
+    {
+        return (T *)Allocate(sizeof(T));
+    }
+
+    template<typename T>
+    T *Allocate(uint32_t count)
+    {
+        return (T *)Allocate(sizeof(T) * count);
+    }
+
+    void *Allocate(uint32_t size)
+    {
+        if (size > m_Size)
+            return nullptr;
+        if (size == 0 || m_Pointer + size - m_Base > m_Size || m_Pointer == nullptr)
+            return nullptr;
+
+        char *ptr = m_Pointer;
+        m_Pointer += size;
+        return (void *)ptr;
+    }
+private:
+    char *m_Base = nullptr;
+    uint32_t m_Size = 0;
+    char *m_Pointer = nullptr;
+};
---
+++ engine/test/ArenaTests.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include "Arena.h"
+
+TEST(Arena, ShouldAllocateMemoryWhenSizeIsWithinLimit)
+{
+    Arena arena(1);
+    auto data = arena.Allocate<uint32_t>(1);
+    ASSERT_NE(data, nullptr);
+}
+
+TEST(Arena, ShouldNotAllocateMemoryWhenSizeExceedsLimit)
+{
+    Arena arena(1);
+    auto data = arena.Allocate(2);
+    ASSERT_EQ(data, nullptr);
+}
+
+TEST(Arena, ShouldInitializeAllocatedMemoryBlockToZero)
+{
+    Arena arena(16);
+    auto data = arena.Allocate<char>(16);
+
+    ASSERT_EQ(*data, 0);
+    ASSERT_EQ(data[15], 0);
+}
+
+TEST(Arena, ShouldNotAllocateMemoryWhenSizeIsZero)
+{
+    Arena arena(1);
+    auto data = arena.Allocate(0);
+    ASSERT_EQ(data, nullptr);
+}
+
+TEST(Arena, ShouldAllocateDifferentTypes)
+{
+    Arena arena(16);
+    auto data1 = arena.Allocate<uint32_t>(1);
+    auto data2 = arena.Allocate<char>(1);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+}
+
+TEST(Arena, ShouldNotAllocateMemoryWhenArenaIsFull)
+{
+    Arena arena(16);
+    auto data1 = arena.Allocate<uint32_t>(1);
+    auto data2 = arena.Allocate<uint32_t>(1);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_EQ(data2, nullptr);
+}
