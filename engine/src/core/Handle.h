f1cc7b3b9130a5616eb1497c39918fe394e43ee3
Willehard Korander <willehard@gmail.com>
2024-08-04 21:05:30 +0300

    Remove commented operators and data member

--- engine/src/core/Handle.h
+++ engine/src/core/Handle.h
@@ -11,17 +11,6 @@ namespace Figment
     struct Handle
     {
         explicit Handle(uint32_t index = 0, uint32_t generation = 0) : index(index), generation(generation) { }
-        // explicit operator T() { return data; }
-        // explicit operator const T() const { return data; }
-        //
-        // explicit operator T &() { return data; }
-        // explicit operator const T &() const { return data; }
-
-        // Equality operators
-        // bool operator==(const Handle<T> &other) const
-        // {
-        //     return index == other.index && generation == other.generation;
-        // }
 
         [[nodiscard]] uint32_t Index() const { return index; }
         [[nodiscard]] uint32_t Generation() const { return generation; }
@@ -31,6 +20,5 @@ namespace Figment
 
         uint32_t index = 0;
         uint32_t generation = 0;
-        T data;
     };
 }
