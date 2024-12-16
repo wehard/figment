59010d5529673d479b4b81e6095079d17a7da0d9
Willehard Korander <willehard@gmail.com>
2024-12-16 19:00:53 +0200

    Updated presenter

--- vk-app/src/main.cpp
+++ vk-app/src/main.cpp
@@ -1,25 +1,50 @@
 #include "context.h"
-#include "vk_imgui_presenter.h"
+#include "vk_debug_tools.h"
 
 using namespace figment;
 
 int main()
 {
     figment::vulkan::Context context;
-    vk_imgui_presenter::VkImGuiPresenter vkImGuiPresenter;
+    vkdt::Presenter presenter;
     context.Init(1280, 720);
+    presenter.init({
+        .width          = 1280,
+        .height         = 720,
+        .instance       = context.GetInstance(),
+        .device         = context.GetDevice(),
+        .physicalDevice = context.GetPhysicalDevice(),
+        .queue          = context.GetGraphicsQueue(),
+        .queueIndex     = 0,
+    });
 
-    vkImGuiPresenter.init(context.GetInstance(), context.GetDevice(), context.GetPhysicalDevice(),
-                          context.GetGraphicsQueue(), 0, 1280, 720);
+    auto imguiRenderer = vkdt::ImGuiRenderer({
+        .window          = presenter.getWindow(),
+        .instance        = context.GetInstance(),
+        .device          = context.GetDevice(),
+        .physicalDevice  = context.GetPhysicalDevice(),
+        .queue           = context.GetGraphicsQueue(),
+        .minImageCount   = 2,
+        .swapchainFormat = presenter.getSurfaceFormat().format,
+    });
 
-    while (true)
+    while (!presenter.shouldClose())
     {
-        vkImGuiPresenter.prepare(context.GetDevice());
+        presenter.prepare();
+        imguiRenderer.begin(presenter.getCommandBuffer(),
+                            {
+                                .image      = presenter.getImage(),
+                                .imageView  = presenter.getImageView(),
+                                .renderArea = {0, 0, presenter.getWindowExtent().width,
+                                               presenter.getWindowExtent().height},
+                            });
         ImGui::Begin("Window");
         ImGui::Text("Hello, world!");
         ImGui::End();
-        vkImGuiPresenter.present(context.GetGraphicsQueue());
+        imguiRenderer.end(presenter.getCommandBuffer());
+        presenter.present();
     }
+    presenter.cleanup();
 
     return 0;
 }
